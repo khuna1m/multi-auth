f33dfe43284f7237aa4f72b4b1ea69fe3f603e63
Ploychanok Jiamchoatpattanakul <>
2021-07-06 16:27:08 +0700

    Changed DB name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Welcome</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap" rel="stylesheet" />
+    
     <style>
         * {
             margin: 0;
